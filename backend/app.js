10481574f98f4caa69f4166d524c4a9e9066c9b8
Mukesh1503 <117158200+Mukesh1503@users.noreply.github.com>
2023-06-05 14:08:17 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(router);
 const PORT = 5000;
 
 
-app.use(express.static(path.join(__dirname,"../build")))
+app.use(express.static(path.join(__dirname,"./build")))
 
 app.listen(PORT, () => {
   console.log("server start");
